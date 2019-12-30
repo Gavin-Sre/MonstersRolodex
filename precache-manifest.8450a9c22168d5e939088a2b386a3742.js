7daebe5ef45d3104f42d2e8aec224fa58e59c806
GavinSree <sreesg@uw.edu>
2019-12-30 15:01:21 +0700

    Updates

---
+++ precache-manifest.8450a9c22168d5e939088a2b386a3742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef146acb3af8aab4301a4ff659064e64",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "8b0e2414172d5ebf8627",
+    "url": "/MonstersRolodex/static/css/main.c5c2e1c9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/MonstersRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonstersRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b0e2414172d5ebf8627",
+    "url": "/MonstersRolodex/static/js/main.b0425991.chunk.js"
+  },
+  {
+    "revision": "e5fbf05eb7b3cf2cb151",
+    "url": "/MonstersRolodex/static/js/runtime-main.2bd3aecc.js"
+  }
+]);
